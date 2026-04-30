f8e2ef5ae45f6976b2f1d0ca1a6b7ab5c2942a6d
departemenikan <departemenikan@gmail.com>
2026-04-30 08:00:45 +0800

    file 59

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs      = require("fs");
 const path    = require("path");
-const bcrypt  = require("bcrypt");
+const bcrypt  = require("bcryptjs");
 const app     = express();
 
 const BCRYPT_ROUNDS = 10;
